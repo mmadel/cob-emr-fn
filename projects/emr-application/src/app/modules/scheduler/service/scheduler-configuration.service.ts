c9113bec21cf157a69ba967fa9560160c334aa32
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-10-23 15:41:31 +0300

    imp scheduler configuration services

--- projects/emr-application/src/app/modules/scheduler/service/scheduler-configuration.service.ts
+++ projects/emr-application/src/app/modules/scheduler/service/scheduler-configuration.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'projects/emr-application/src/environments/environment';
+import { SchedulerConfiguration } from '../models/configuration';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SchedulerConfigurationService {
+  private baseUrl = environment.baseURL;
+  constructor(private _http: HttpClient) { }
+  create(clinicSchedulerConfiguration: SchedulerConfiguration) {
+    const createClinicSchedulerConfigurationURL = this.baseUrl + 'scheduler/configuration';
+    return this._http.post(createClinicSchedulerConfigurationURL, JSON.stringify(clinicSchedulerConfiguration));
+  }
 
-  constructor() { }
+  update(clinicSchedulerConfiguration: SchedulerConfiguration) {
+    const createClinicSchedulerConfigurationURL = this.baseUrl + 'scheduler/configuration';
+    return this._http.put(createClinicSchedulerConfigurationURL, JSON.stringify(clinicSchedulerConfiguration));
+  }
+
+  retrieveCliniSchedulerConfigurationById(id: number) {
+    const listClinicSchedulerConfigurationURL = this.baseUrl + 'scheduler/configuration/find/id/' + id;
+    return this._http.get<SchedulerConfiguration>(listClinicSchedulerConfigurationURL);
+  }
+
+  retrieveClinicSchedulerConfigurations(uuid: string) {
+    const listClinicSchedulerConfigurationURL = this.baseUrl + 'scheduler/configuration/find/uuid/' + uuid;
+    return this._http.get(listClinicSchedulerConfigurationURL);
+  }
+  retrieveCliniSchedulerConfigurationByClinicId(clinicId: number, organizationId: number) {
+    const listClinicSchedulerConfigurationURL = this.baseUrl + 'scheduler/configuration/find/clinicId/' + clinicId + '/organization/' + organizationId;
+    return this._http.get<SchedulerConfiguration>(listClinicSchedulerConfigurationURL);
+  }
 }
