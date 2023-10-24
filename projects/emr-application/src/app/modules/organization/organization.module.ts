fda8bb247827062da29dc038b57094b7bf4af518
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-10-24 09:11:54 +0300

    create list and create organization components skeleton

--- projects/emr-application/src/app/modules/organization/organization.module.ts
+++ projects/emr-application/src/app/modules/organization/organization.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrganizationRoutingModule } from './organization-routing.module';
+import { CreateOrganizationComponent } from './components/create/create-organization.component';
+import { ListOrganizationComponent } from './components/list/list-organization.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateOrganizationComponent,
+    ListOrganizationComponent
+  ],
   imports: [
     CommonModule,
     OrganizationRoutingModule
