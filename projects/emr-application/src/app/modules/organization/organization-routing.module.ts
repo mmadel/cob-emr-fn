556cb61cee7f10b6cc3f94bca69d9d385e0290a6
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-10-24 09:23:40 +0300

    create Organization routing

--- projects/emr-application/src/app/modules/organization/organization-routing.module.ts
+++ projects/emr-application/src/app/modules/organization/organization-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateOrganizationComponent } from './components/create/create-organization.component';
+import { ListOrganizationComponent } from './components/list/list-organization.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Organization',
+    },
+    children: [
+      {
+        path: 'create',
+        component: CreateOrganizationComponent,
+        data: {
+          title: 'create',
+        },
+      },
+      {
+        path: 'list',
+        component: ListOrganizationComponent,
+        data: {
+          title: 'Organizations',
+        },
+
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
