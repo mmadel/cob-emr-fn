0ea426fdf16f9ee2810b60956b3d462e1c15f825
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-11-06 19:57:30 +0200

    construct the address of the clinic

--- projects/emr-application/src/app/modules/administration/components/clinic/list/list-clinic.component.ts
+++ projects/emr-application/src/app/modules/administration/components/clinic/list/list-clinic.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { IColumn } from '@coreui/angular-pro/lib/smart-table/smart-table.type';
 import { ToastrService } from 'ngx-toastr';
-import { Observable } from 'rxjs';
+import { map, Observable, retry, tap } from 'rxjs';
+import { Address } from '../../../../common/models';
 import { ListTemplate } from '../../../../common/template/list.template';
 import { Clinic } from '../../../../patient/models/clinic';
 import { ClinicService } from '../../../services/clinic/clinic.service';
@@ -15,19 +17,52 @@ import { ClinicService } from '../../../services/clinic/clinic.service';
 export class ListClinicComponent extends ListTemplate implements OnInit {
   clinics$!: Observable<Clinic[]>;
   columns: (string | IColumn)[];
-  constructor(private router: Router 
-    ,private clinicService:ClinicService
-    ,private toastr: ToastrService)
-     {super(); }
+  constructor(private router: Router
+    , private clinicService: ClinicService
+    , private toastr: ToastrService
+    , private sanitizer: DomSanitizer) { super(); }
 
   ngOnInit(): void {
-    this.columns = this.constructColumns(['name', 'insuranceType', 'phone', 'fax', 'actions']);
+    this.columns = this.constructColumns(['name', 'address', 'actions']);
     this.initListComponent();
+    this.clinics$ = this.clinicService.get(this.apiParams$).pipe(
+      retry({
+        delay: (error) => {
+          console.warn('Retry: ', error);
+          this.errorMessage$.next(error.message ?? `Error: ${JSON.stringify(error)}`);
+          this.loadingData$.next(false);
+          return this.retry$;
+        }
+      }),
+      tap((response: any) => {
+        this.totalItems$.next(response.number_of_matching_records);
+        if (response.number_of_records) {
+          this.errorMessage$.next('');
+        }
+        this.retry$.next(false);
+        this.loadingData$.next(false);
+      }),
+      map((response: any) => {
+        return response.records;
+      })
+    );
   }
   create() {
     this.router.navigateByUrl('emr/administration/create/clinic');
   }
   remove(item: any) {
-   
+
+  }
+  constructAddress(billingAddress: Address) :string{
+    var result: string = '';
+    for (const [key, value] of Object.entries(billingAddress)) {
+      if (key === 'firstAddress')
+        result = value + ',';
+      if (key === 'state')
+        result = value + ',';
+      if (key === 'zipCode')
+        result = value;
+    }
+    return result;
   }
 }
--- projects/emr-application/src/app/modules/administration/services/clinic/clinic.service.ts
+++ projects/emr-application/src/app/modules/administration/services/clinic/clinic.service.ts
@@ -1,9 +1,17 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'projects/emr-application/src/environments/environment';
-import { BehaviorSubject, map } from 'rxjs';
+import { BehaviorSubject, catchError, debounceTime, distinctUntilChanged, map, Observable, retry, switchMap, throwError } from 'rxjs';
+import { IApiParams } from '../../../common/interfaces/api.params';
+import { IData } from '../../../patient/components/list/interfaces/i.data';
 import { Clinic } from '../../../patient/models/clinic';
-
+const httpOptions = {
+  // headers: new HttpHeaders({
+  //   'Content-Type': 'application/json',
+  //   'Access-Control-Allow-Origin': '*',
+  //   Connection: 'keep-alive'
+  // })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -11,20 +19,50 @@ import { Clinic } from '../../../patient/models/clinic';
 export class ClinicService {
   public selectedClinic$: BehaviorSubject<number | null> = new BehaviorSubject<number | null>(null);
   private userUrl = environment.baseURL + 'clinic'
-  constructor(private httpClient:HttpClient) { }
-  
-  create(clinic:Clinic){
+  constructor(private httpClient: HttpClient) { }
+
+  create(clinic: Clinic) {
     const headers = { 'content-type': 'application/json' }
     var createURL = this.userUrl + '/create'
-    return this.httpClient.post(`${createURL}`, JSON.stringify(clinic), { 'headers': headers})
+    return this.httpClient.post(`${createURL}`, JSON.stringify(clinic), { 'headers': headers })
   }
   getByUserId(userId: string | undefined) {
-    const url = this.userUrl + '/find'+'/clinics/userUUID/' + userId;
+    const url = this.userUrl + '/find' + '/clinics/userUUID/' + userId;
     return this.httpClient.get(url).pipe(
       map((response: any) => <Clinic[]>response));
   }
-  delete(id:number){
-    
+  delete(id: number) {
+
+  }
+  get(config$: BehaviorSubject<IApiParams>): Observable<any> {
+    return config$.pipe(
+      debounceTime(100),
+      distinctUntilChanged(
+        (previous, current) => {
+          return JSON.stringify(previous) === JSON.stringify(current);
+        }
+      ),
+      switchMap((config) => this.fetchData(config))
+    );
+  }
+  private fetchData(params: IApiParams): Observable<IData> {
+    const apiParams = {
+      ...params
+    };
+    const httpParams: HttpParams = new HttpParams({ fromObject: apiParams });
+
+    const options = Object.keys(httpParams).length
+      ? { params: httpParams, ...httpOptions }
+      : { params: {}, ...httpOptions };
+    return this.httpClient
+      .get<IData>(this.userUrl + "/find/organization/" + 1, options)
+      .pipe(
+        retry({ count: 1, delay: 100000, resetOnSuccess: true }),
+        catchError(this.handleHttpError)
+      );
+  }
+  private handleHttpError(error: HttpErrorResponse) {
+    return throwError(() => error);
   }
 
 }
