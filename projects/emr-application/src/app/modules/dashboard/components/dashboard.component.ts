7b6a601e39270882251bc592ef768167b0d8fe0a
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-11-07 15:58:01 +0200

    add encryption and create encryption service

--- projects/emr-application/src/app/modules/dashboard/components/dashboard.component.ts
+++ projects/emr-application/src/app/modules/dashboard/components/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EncryptService } from '../../common/service/encyrption/encrypt.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private encryptService:EncryptService) { }
 
   ngOnInit(): void {
+    console.log(this.encryptService.encrypt('Khaled@123'))
   }
 
 }
