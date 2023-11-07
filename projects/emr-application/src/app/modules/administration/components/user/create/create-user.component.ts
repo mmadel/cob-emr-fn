0a94cb55db63652dc6f09dea5f1d043c51278bad
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-11-07 17:11:53 +0200

    imple the select role in user creation screen

--- projects/emr-application/src/app/modules/administration/components/user/create/create-user.component.ts
+++ projects/emr-application/src/app/modules/administration/components/user/create/create-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Roles } from '../../../../common/models/enums/roles';
 import { User } from '../../../model/user/user';
 
 @Component({
@@ -9,8 +10,9 @@ import { User } from '../../../model/user/user';
 })
 export class CreateUserComponent implements OnInit {
   @ViewChild('userCreateForm') userCreateForm: NgForm;
+  roles = Roles;
   user:User={
-    
+    role:null
   }
   constructor() { }
 
---
+++ projects/emr-application/src/app/modules/common/models/enums/roles.ts
@@ -0,0 +1,6 @@
+export enum Roles{
+    administration_emr_role="Administration",
+    clinical_emr_role="Clinical",
+    billing_emr_role="Billing",
+    clerical_emr_role="Front Desk",
+}
