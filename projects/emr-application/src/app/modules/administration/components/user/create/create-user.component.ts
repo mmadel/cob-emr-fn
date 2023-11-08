1b61b1282775095bb85c55227835a01674d815a5
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-11-08 09:04:54 +0200

    impl create user service

--- projects/emr-application/src/app/modules/administration/components/user/create/create-user.component.ts
+++ projects/emr-application/src/app/modules/administration/components/user/create/create-user.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { from, switchMap } from 'rxjs';
 import { Roles } from '../../../../common/models/enums/roles';
 import { CacheService } from '../../../../common/service/cahce/cache.service';
+import { EncryptService } from '../../../../common/service/encyrption/encrypt.service';
 import { Clinic } from '../../../../patient/models/clinic';
 import { User } from '../../../model/user/user';
 import { ClinicService } from '../../../services/clinic/clinic.service';
+import { UserService } from '../../../services/user/user.service';
 
 @Component({
   selector: 'app-create-user',
@@ -14,27 +18,46 @@ import { ClinicService } from '../../../services/clinic/clinic.service';
 })
 export class CreateUserComponent implements OnInit {
   @ViewChild('userCreateForm') userCreateForm: NgForm;
+  submitted: boolean = false;
+  validAddress: boolean = true;
   roles = Roles;
-  clinics:Clinic[];
+  clinics: Clinic[];
   user: User = {
     role: null,
     clinics: []
   }
-  constructor(private clinicService: ClinicService, private cacheService: CacheService) { }
+  constructor(private clinicService: ClinicService
+    , private cacheService: CacheService
+    , private userService: UserService
+    , private toastr: ToastrService
+    , private router: Router
+    , private encryptService: EncryptService) { }
 
   ngOnInit(): void {
     from(this.cacheService.getOrganizationId())
-    .pipe(
-      switchMap(result => this.clinicService.getByOrganizationId(result))
-    ).subscribe((response:any)=>{
-      this.clinics = response.records;
-      console.log(this.clinics)
-    })
+      .pipe(
+        switchMap(result => this.clinicService.getByOrganizationId(result))
+      ).subscribe((response: any) => {
+        this.clinics = response.records;
+      })
   }
   create() {
-
+    if (this.userCreateForm.valid) {
+      this.submitted = false;
+      var encryptedPassword = this.encryptService.encrypt(this.user.password);
+      this.user.password = encryptedPassword
+      this.userService.create(this.user).subscribe(result => {
+        this.toastr.success('Clinic Created');
+        this.router.navigateByUrl('emr/administration/list/user')
+      }, (error) => {
+        console.log(error);
+        this.toastr.error(error.error.message, 'Error In Creation');
+      })
+    } else {
+      this.submitted = true;
+    }
   }
   resetError() {
-
+    this.submitted = false;
   }
 }
---
+++ projects/emr-application/src/app/modules/administration/services/user/user.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'projects/emr-application/src/environments/environment';
+import { User } from '../../model/user/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private userUrl = environment.baseURL + 'user'
+  constructor(private httpClient: HttpClient) { }
+  create(user: User){
+    const headers = { 'content-type': 'application/json' }
+    var createURL = this.userUrl + '/create'
+    return this.httpClient.post(`${createURL}`, JSON.stringify(user), { 'headers': headers })
+  }
+}
